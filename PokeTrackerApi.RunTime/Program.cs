6fefeb2aaddad885f1264f7dd8bc875e5e1fdbbb
mbargiel <mbargiel@btopservice.net>
2025-08-02 13:51:43 +0200

    Delete previous workflow

--- PokeTrackerApi.RunTime/Program.cs
+++ PokeTrackerApi.RunTime/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy => policy.AllowAnyMethod()
         .AllowAnyHeader()
-        .WithOrigins("http://localhost:5173", "https://lively-bay-073042e10.1.azurestaticapps.net"));
+        .WithOrigins("http://localhost:5173", "http://localhost:5174", "https://lively-bay-073042e10.1.azurestaticapps.net"));
 });
 
 var app = builder.Build();
